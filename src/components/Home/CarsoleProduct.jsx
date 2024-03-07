26cc8f8bf11b0af4be2664675a680d92d0dd47c0
Hrishi-jain <82449850+Hrishi-jain@users.noreply.github.com>
2024-03-07 16:46:01 +0530

    Hrishi changes

--- src/components/Home/CarsoleProduct.jsx
+++ src/components/Home/CarsoleProduct.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 // Prasjahtjtjtgit stat
+// Hrishi changes
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
